4f6eb76ea37775315dd0a31af34f4758a31da644
Sebastian Winberg <winbergs@student.ethz.ch>
2020-06-17 14:26:50 +0200

    Made changes for tf-gpu

--- data.py
+++ data.py
@@ -10,30 +10,33 @@ train_path = 'training/training'
 test_path = 'test_images'
 result_path = 'test_images/results'
 
-def trainGenerator():
-	image_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size = 4)
-	mask_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['groundtruth'], class_mode = None, color_mode = 'grayscale', target_size = (400,400), batch_size = 4)
+def trainGenerator(batch_size = 4):
+	image_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size=batch_size)
+	mask_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['groundtruth'], class_mode = None, color_mode = 'grayscale', target_size = (400,400), batch_size=batch_size)
 	train_generator = zip(image_generator, mask_generator)
-	for (img,mask) in train_generator:
-		mask = mask / np.max(mask)
-		mask[mask > 0.5] = 1
-		mask[mask <= 0.5] = 0
-		yield(img / 255, mask)
+	for (img, mask) in train_generator:
+		mask = mask / 255.0
+		mask[mask > 0.5] = 1.0
+		mask[mask <= 0.5] = 0.0
+		img = img / 255.0
+		yield(img, mask)
 
-def validationGenerator():
-	image_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['validation_images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size = 2)
-	mask_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['validation_groundtruth'], class_mode = None, color_mode = 'grayscale', target_size = (400,400), batch_size = 2)
+def validationGenerator(batch_size = 2):
+	image_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['validation_images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size=batch_size)
+	mask_generator = ImageDataGenerator().flow_from_directory(train_path, classes = ['validation_groundtruth'], class_mode = None, color_mode = 'grayscale', target_size = (400,400), batch_size=batch_size)
 	validation_generator = zip(image_generator, mask_generator)
 	for (img,mask) in validation_generator:
-		mask = mask / np.max(mask)
-		mask[mask > 0.5] = 1
-		mask[mask <= 0.5] = 0
-		yield(img / 255, mask)
+		mask = mask / 255.0
+		mask[mask > 0.5] = 1.0
+		mask[mask <= 0.5] = 0.0
+		img = img / 255.0
+		yield(img, mask)
 
 def testGenerator():
-	test_generator = ImageDataGenerator().flow_from_directory(test_path, classes = ['test_images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size = 2)
+	test_generator = ImageDataGenerator().flow_from_directory(test_path, classes = ['test_images'], class_mode = None, color_mode = 'rgb', target_size = (400,400), batch_size = 4)
 	for img in test_generator:
-		yield img / 255
+		img = img / 255.0
+		yield img
 
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
--- main.py
+++ main.py
@@ -10,9 +10,9 @@ train_batch = trainGenerator()
 validation_batch = validationGenerator()
 
 model = unet()
-model.fit_generator(train_batch, steps_per_epoch=10, validation_data=validation_batch, validation_steps=2, epochs=3, verbose=1)
+model.fit_generator(train_batch, steps_per_epoch=100, validation_data=validation_batch, validation_steps=2, epochs=500, verbose=1)
 
 test_batch = testGenerator()
-results = model.predict_generator(test_batch ,2,verbose=1)
-results = results * 255
-saveResult("test_images/results",results)
+results = model.predict_generator(test_batch, 2, verbose=1)
+results = results
+saveResult("test_images/results", results)
