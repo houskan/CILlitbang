b2cb7ed4cafef8ce45d01aad12dc0c3b068adad9
Sebastian Winberg <winbergs@student.ethz.ch>
2020-07-10 01:09:35 +0200

    Added some new flags and added first version of combined result prediction

--- src/main.py
+++ src/main.py
@@ -11,11 +11,15 @@ import data.data
 from models.model import *
 from data.data import *
 from data.tensorboard_image import *
+from data.post_processing import *
 
 predict_best = True
+train = True
+combined_prediction = True
 
 train_path = '../data/training/'
 test_path = '../data/test/'
+model_path = "../tmp/model.h5"
 
 # Augmentation parameters for training generator (not validation!)
 data_gen_args = dict(rotation_range=45,
@@ -35,38 +39,39 @@ trainGen, valGen = getTrainGenerators(data_gen_args, train_path=train_path,
 # Initializing and compiling unet model
 model = unet()
 
-# initializing callbacks for training
-callbacks = []
+if train:
+    # initializing callbacks for training
+    callbacks = []
 
-# Tensorboard initialization
-log_dir = "../logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=True)
-callbacks.append(tensorboard_callback)
+    # Initializing tensorboard callback for plots, graph, etc.
+    log_dir = "../logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, write_graph=True)
+    callbacks.append(tensorboard_callback)
 
-# Tensorboard image initialization
-tensorboard_image = TensorBoardImage(log_dir=log_dir, validation_pairs=data.data.validation_pairs)
-callbacks.append(tensorboard_image)
+    # Initializing tensorboard image callback for visualizing validation images each epoch
+    tensorboard_image_callback = TensorBoardImage(log_dir=log_dir, validation_pairs=data.data.validation_pairs)
+    callbacks.append(tensorboard_image_callback)
 
-# Checkpoint initialization to store best model
-model_path = "../tmp/model.h5"
-checkpoint_callback = ModelCheckpoint(model_path, monitor="val_loss", mode="min", save_best_only=True, verbose=1)
-callbacks.append(checkpoint_callback)
+    # Initialization model checkpoint to store model with best validation loss
+    model_checkpoint_callback = ModelCheckpoint(model_path, monitor="val_loss", mode="min", save_best_only=True, verbose=1)
+    callbacks.append(model_checkpoint_callback)
 
-# Training unet model
-model.fit(trainGen, steps_per_epoch=100, epochs=50, validation_data=valGen, validation_steps=10, callbacks=callbacks, verbose=1)
-
-# Initializing test generator
-testGen = testGenerator(test_path=test_path, image_folder='images', target_size=(400, 400))
+    # Training unet model
+    model.fit(trainGen, steps_per_epoch=100, epochs=50, validation_data=valGen, validation_steps=10, callbacks=callbacks, verbose=1)
 
 # Checking if model weights for best val_loss should be picked for prediction
 if predict_best:
     # Loading best result
     model.load_weights(model_path)
 
-# Predicting results on test images
-images = os.listdir(os.path.join(test_path, 'images'))
-results = model.predict(testGen, steps=len(images), verbose=1)
-
-# Saving result masks of test images
-saveResult(test_path=test_path, images=images, results=results)
-
+if combined_prediction:
+    # Saving result masks of test images
+    saveCombinedResult(model=model, test_path=test_path, image_folder='images')
+else:
+    # Initializing test generator
+    testGen = testGenerator(test_path=test_path, image_folder='images', target_size=(400, 400))
+    # Predicting results on test images
+    images = os.listdir(os.path.join(test_path, 'images'))
+    results = model.predict(testGen, steps=len(images), verbose=1)
+    # Saving result masks of test images
+    saveResult(test_path=test_path, images=images, results=results)
