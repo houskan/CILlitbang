7321c31959e3626df0cf267666397957d9345adf
Sebastian Winberg <winbergs@student.ethz.ch>
2020-07-21 12:38:26 +0200

    Added foreground threshold as argument to mask_to_submission

--- src/data/mask_to_submission.py
+++ src/data/mask_to_submission.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.image as mpimg
 import re
 
-foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
+#foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
 
 # assign a label to a patch
-def patch_to_label(patch):
+def patch_to_label(patch, foreground_threshold):
     df = np.mean(patch)
     if df > foreground_threshold:
         return 1
@@ -16,7 +16,7 @@ def patch_to_label(patch):
         return 0
 
 
-def mask_to_submission_strings(image_filename):
+def mask_to_submission_strings(image_filename, foreground_threshold):
     """Reads a single image and outputs the strings that should go into the submission file"""
     img_number = int(re.search(r"\d+", os.path.basename(image_filename)).group(0))
     im = mpimg.imread(image_filename)
@@ -24,24 +24,24 @@ def mask_to_submission_strings(image_filename):
     for j in range(0, im.shape[1], patch_size):
         for i in range(0, im.shape[0], patch_size):
             patch = im[i:i + patch_size, j:j + patch_size]
-            label = patch_to_label(patch)
+            label = patch_to_label(patch, foreground_threshold)
             yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
 
 
-def masks_to_submission(submission_filename, *image_filenames):
+def masks_to_submission(submission_filename, *image_filenames, foreground_threshold):
     """Converts images into a submission file"""
     with open(submission_filename, 'w') as f:
         f.write('id,prediction\n')
         for fn in image_filenames[0:]:
-            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn))
+            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn, foreground_threshold))
 
 
 if __name__ == '__main__':
-	result_path = '../../data/test/results'
-	submission_filename = 'submission.csv'
-	image_filenames = []
-	for file in os.listdir(result_path):
-		image_filename = os.path.join(result_path, file)
-		print(image_filename)
-		image_filenames.append(image_filename)
-	masks_to_submission(submission_filename, *image_filenames)
+    result_path = '../../data/test/results'
+    submission_filename = 'submission.csv'
+    image_filenames = []
+    for file in os.listdir(result_path):
+        image_filename = os.path.join(result_path, file)
+        print(image_filename)
+        image_filenames.append(image_filename)
+    masks_to_submission(submission_filename, *image_filenames, foreground_threshold=0.5)
