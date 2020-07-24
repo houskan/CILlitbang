847896c8b8deadfed22a0a2da2e6bd4f34bb655d
Steven Stalder <steven.stalder30@gmail.com>
2020-07-24 12:50:14 +0200

    Added keras-segmentation baseline

---
+++ src/baselines/keras_segmentation/keras_seg_submission.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import os
+import numpy as np
+import matplotlib.image as mpimg
+import re
+
+# assign a label to a patch
+def patch_to_label(patch, foreground_threshold):
+    df = np.mean(patch)
+    if df > foreground_threshold:
+        return 1
+    else:
+        return 0
+
+
+def mask_to_submission_strings(image_filename, foreground_threshold):
+    """Reads a single image and outputs the strings that should go into the submission file"""
+    img_number = int(re.search(r"\d+", os.path.basename(image_filename)).group(0))
+    im = mpimg.imread(image_filename)
+    patch_size = 16
+    for j in range(0, im.shape[1], patch_size):
+        for i in range(0, im.shape[0], patch_size):
+            patch = im[i:i + patch_size, j:j + patch_size]
+            label = patch_to_label(patch, foreground_threshold)
+            yield("{:03d}_{}_{},{}".format(img_number, j, i, label))
+
+
+def masks_to_submission(submission_filename, *image_filenames, foreground_threshold):
+    """Converts images into a submission file"""
+    with open(submission_filename, 'w') as f:
+        f.write('id,prediction\n')
+        for fn in image_filenames[0:]:
+            f.writelines('{}\n'.format(s) for s in mask_to_submission_strings(fn, foreground_threshold))
+
+
+def create_submission():
+    result_path = './results'
+    submission_filename = 'keras_seg_submission.csv'
+    image_filenames = []
+    for file in os.listdir(result_path):
+        image_filename = os.path.join(result_path, file)
+        print(image_filename)
+        image_filenames.append(image_filename)
+    masks_to_submission(submission_filename, *image_filenames, foreground_threshold=0.5)
