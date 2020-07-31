90e11f355cc7222c2ccf20d17513106e5e464d0c
Sebastian Winberg <winbergs@student.ethz.ch>
2020-07-31 11:21:43 +0200

    Slight cleanup of loss_functions.py

--- src/models/loss_functions.py
+++ src/models/loss_functions.py
@@ -6,14 +6,8 @@ This file contains multiple loss functions with which we experimented with
 '''
 
 # Initializing foreground threshold (percentage of road pixels s.t. patch is considered road)
-foreground_threshold = 0.25
+foreground_threshold = 0.5
 
-def dice_coef(y_true, y_pred, smooth=1):
-    y_true_f = tf.reshape(y_true, [-1])
-    y_pred_f = tf.reshape(y_pred, [-1])
-    intersection = tf.reduce_sum(y_true_f * y_pred_f, axis=0)
-    dice = (2.0 * intersection + smooth) / (tf.reduce_sum(y_true_f, axis=0) + tf.reduce_sum(y_pred_f,  axis=0) + smooth)
-    return dice
 
 def dice_loss(y_true, y_pred):
     y_true_f = tf.reshape(y_true, [-1])
@@ -24,8 +18,6 @@ def dice_loss(y_true, y_pred):
 
     return 1.0 - (numerator + 1.0) / (denominator + 1.0)
 
-def soft_dice_loss(y_true, y_pred):
-    return 1.0 - dice_coef(y_true, y_pred)
 
 def iou_coef(y_true, y_pred, smooth=1):
     intersection = tf.reduce_sum(tf.abs(y_true * y_pred), axis=[1, 2, 3])
@@ -33,14 +25,15 @@ def iou_coef(y_true, y_pred, smooth=1):
     iou = tf.reduce_mean((intersection + smooth) / (union + smooth), axis=0)
     return iou
 
+
 def iou_loss_log(y_true, y_pred):
     intersection = tf.reduce_sum(y_true * y_pred, axis=[1, 2, 3])
     union = tf.reduce_sum(y_true + y_pred, axis=[1, 2, 3]) - intersection
     iou = tf.reduce_mean(intersection / union, axis=0)
     return -tf.math.log(iou)
 
-def patch_f1_score(y_true, y_pred):
 
+def patch_f1_score(y_true, y_pred):
     # Initializing ones kernel for summing up patch mean
     ones = tf.ones([16, 16, 1, 1])
 
@@ -67,6 +60,7 @@ def patch_f1_score(y_true, y_pred):
     f1 = 2.0 * precision * recall / (precision + recall)
     return f1
 
+
 def patch_loss(y_true, y_pred):
     # Initializing ones kernel for summing up patch mean
     ones = tf.ones([16, 16, 1, 1])
@@ -86,17 +80,15 @@ def patch_loss(y_true, y_pred):
 
 #https://lars76.github.io/neural-networks/object-detection/losses-for-segmentation/
 def weighted_cross_entropy(beta):
-  def convert_to_logits(y_pred):
-      # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
-      y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+    def convert_to_logits(y_pred):
+        # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
+        y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+        return tf.math.log(y_pred / (1 - y_pred))
 
-      return tf.math.log(y_pred / (1 - y_pred))
+    def loss(y_true, y_pred):
+        y_pred = convert_to_logits(y_pred)
+        loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, labels=y_true, pos_weight=beta)
+        # or reduce_sum and/or axis=-1
+        return tf.reduce_mean(loss)
 
-  def loss(y_true, y_pred):
-    y_pred = convert_to_logits(y_pred)
-    loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, labels=y_true, pos_weight=beta)
-
-    # or reduce_sum and/or axis=-1
-    return tf.reduce_mean(loss)
-
-  return loss
+    return loss
